31fdd3184778a1f880f8de507797bc518be74bde
Donisetti Ferreira Cosma <donisetti@vegasoft.com.br>
2018-05-06 22:34:20 -0300

    Ajuste no Controller Especialidade

--- Clinica.API/Controllers/EspecialidadesController.cs
+++ Clinica.API/Controllers/EspecialidadesController.cs
@@ -14,13 +14,75 @@ namespace Clinica.API.Controllers
     public class EspecialidadesController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+         private readonly DataContexto _contexto;
+
+
         public EspecialidadesController(IUnitOfWork unitOfWork)
         {
+            _contexto = (DataContexto) unitOfWork.Contexto;
             _unitOfWork = unitOfWork;
         }
-        
 
+ // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var especialidades = await _contexto.Especialidades.ToListAsync();
+            return Ok(especialidades);
+
+        }
+      
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var especialidade = await _contexto.Especialidades.FirstOrDefaultAsync(x => x.ID == id);
+            return Ok(especialidade);
+        }
         
+         // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]Especialidade especialidade)
+        {
+            await _contexto.Especialidades.AddAsync(especialidade);
+            await _contexto.SaveChangesAsync();
+            
+             return Ok(especialidade);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody]Especialidade especialidade)
+        {
+            var _especialidade = await _contexto.Especialidades.FirstOrDefaultAsync(x => x.ID == id);
+
+           if(_especialidade == null) 
+                return NotFound();
+
+            _especialidade.Nome = especialidade.Nome;
+            _especialidade.Ativo = especialidade.Ativo;
+            _especialidade.DataCriacao = especialidade.DataCriacao;
+
+            //await _contexto.Especialidades.
+            await _contexto.SaveChangesAsync();
+            
+             return Ok(especialidade);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var _especialidade =  _contexto.Especialidades.FirstOrDefault(x => x.ID == id);
+
+           if(_especialidade == null) 
+                return NotFound();
+
+             _contexto.Especialidades.Remove(_especialidade);
+             _contexto.SaveChanges();
+            
+             return Ok();
+        }
         
       
     }
