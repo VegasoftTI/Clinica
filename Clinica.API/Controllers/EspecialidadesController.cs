d65032b09664a917a996c543eecd91cdc39d75ad
Donisetti Ferreira Cosma <donisetti@vegasoft.com.br>
2018-05-06 22:22:06 -0300

    Configuração Injeção Dependencia Repositório

--- Clinica.API/Controllers/EspecialidadesController.cs
+++ Clinica.API/Controllers/EspecialidadesController.cs
@@ -4,88 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Clinica.API.Data;
 using Clinica.API.Models;
+using Clinica.API.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Clinica.API.Controllers
 {
-      [Route("api/[controller]")]
+    [Route("api/[controller]")]
     public class EspecialidadesController : Controller
     {
-        private readonly DataContexto _contexto;
-
-        public EspecialidadesController(DataContexto contexto)
+        private readonly IUnitOfWork _unitOfWork;
+        public EspecialidadesController(IUnitOfWork unitOfWork)
         {
-            _contexto = contexto;
+            _unitOfWork = unitOfWork;
         }
         
-        // GET api/values
-        [HttpGet]
-        public async Task<IActionResult> Get()
-        {
-            var especialidades = await _contexto.Especialidades.ToListAsync();
-            return Ok(especialidades);
 
-        }
+        
+        
       
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
-        {
-            var especialidade = await _contexto.Especialidades.FirstOrDefaultAsync(x => x.ID == id);
-            return Ok(especialidade);
-        }
-        
-        // GET api/values/5       
-        /*   [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return Lista[id];
-        } */
-
-        // POST api/values
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody]Especialidade especialidade)
-        {
-            await _contexto.Especialidades.AddAsync(especialidade);
-            await _contexto.SaveChangesAsync();
-            
-             return Ok(especialidade);
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromBody]Especialidade especialidade)
-        {
-            var _especialidade = await _contexto.Especialidades.FirstOrDefaultAsync(x => x.ID == id);
-
-           if(_especialidade == null) 
-                return NotFound();
-
-            _especialidade.Nome = especialidade.Nome;
-            _especialidade.Ativo = especialidade.Ativo;
-            _especialidade.DataCriacao = especialidade.DataCriacao;
-
-            //await _contexto.Especialidades.
-            await _contexto.SaveChangesAsync();
-            
-             return Ok(especialidade);
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            var _especialidade =  _contexto.Especialidades.FirstOrDefault(x => x.ID == id);
-
-           if(_especialidade == null) 
-                return NotFound();
-
-             _contexto.Especialidades.Remove(_especialidade);
-             _contexto.SaveChanges();
-            
-             return Ok();
-        }
-
     }
 }
--- Clinica.API/Startup.cs
+++ Clinica.API/Startup.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Clinica.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +9,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Clinica.API.Data;
+using Clinica.Data.DependencyInjection;
+
 
 namespace Clinica.API
 {
@@ -25,13 +27,31 @@ namespace Clinica.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContexto>(x => x.UseSqlite("Data Source=Clinica.db"));
+            // Use the Threenine.Data Dependency Injection to set up the Unit of Work
+           //services.AddDbContext<DataContexto>(x => x.UseSqlServer(Configuration.GetConnectionString("ClinicaDB"))).AddUnitOfWork<DataContexto>();
+
+            services.AddDbContext<DataContexto>(x => x.UseSqlite("Data Source=Clinica.db"))
+                    .AddUnitOfWork<DataContexto>();
+
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+
+            // Use Threenine.Map to wire up the Automapper mappings
+            //MapConfigurationFactory.Scan<Startup>();
+
+            // Ensure the Database is created
+            /* 
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<DataContexto>().Database.EnsureCreated();
+            } 
+            */
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
